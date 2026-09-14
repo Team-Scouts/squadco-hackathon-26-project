a14d0c2c794adff4d863a9f4371b628e0dc67c93
David Atem <115562286+vilio316@users.noreply.github.com>
2026-09-14 22:49:04 +0100

    added new FE URL to BE cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
       'http://localhost:5190',
       'http://localhost:3000',
       'https://squadco-hackathon-26-project.vercel.app',
+      'https://fraudlens-stable-version.vercel.app'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
